74a9e0b09b5752e97e6026653fb3c8917f8b62b6
Aditya Poduval <adipoduval5@gmail.com>
2025-07-18 23:09:19 +0530

    added shadcn

--- JSON_Schema_Builder/src/main.tsx
+++ JSON_Schema_Builder/src/main.tsx
@@ -1,10 +1,8 @@
 
 import { createRoot } from 'react-dom/client'
 import './index.css'
-
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
-  <h1 class="text-3xl font-bold">
-    Hello world!
-  </h1>
+ <App></App>
 )
